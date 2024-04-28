986a772f61973b5cdb5ccde193c9b6e8c2289bb8
kaikeventura <kaike.gnr@hotmail.com>
2024-04-28 15:20:05 -0300

    Criado classe para chamar o fluxo para lidar com os status das faturas

--- src/main/kotlin/com/kaikeventura/expensemanager/ExpenseManagerApplication.kt
+++ src/main/kotlin/com/kaikeventura/expensemanager/ExpenseManagerApplication.kt
@@ -1,11 +1,25 @@
 package com.kaikeventura.expensemanager
 
+import com.kaikeventura.expensemanager.service.InvoiceService
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class ExpenseManagerApplication
 
 fun main(args: Array<String>) {
 	runApplication<ExpenseManagerApplication>(*args)
+}
+
+@Component
+class Init(
+	private val invoiceService: InvoiceService
+) {
+
+	@PostConstruct
+	fun handleInvoiceCycles() {
+		invoiceService.handleInvoiceCycles()
+	}
 }
